b51303f9bcaa8e986058f2108b90c0761dd6c8f0
Phil Worthington <pworthington27@gmail.com>
2013-10-10 14:28:09 -0700

    Use bootstrap with less for product store.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BootstrapStore::Application.config.secret_key_base = 'b2c8af8a21c8e2cc0173479da1ea1716d5ce058bddf1e0874005c999dde83ca761a8f9a6af1e7b70f242fdd731b6fb8de26e3d67ad4862088ba17b14fbf6d092'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BootstrapStore::Application.config.session_store :cookie_store, key: '_bootstrap-store_session'
